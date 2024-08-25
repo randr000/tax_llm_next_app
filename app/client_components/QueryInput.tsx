e2e44285085fd3842400f8a58aad27a364c685d0
Raul Andrial <rauljandrial@gmail.com>
2024-08-25 08:17:26 -0400

    can successfully retrieve response after sending query locally

--- app/client_components/QueryInput.tsx
+++ app/client_components/QueryInput.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SendIcon from '../server_components/icons/SendIcon';
 
 const QueryInput = ({setChatBubbles}: {setChatBubbles: Function}) => {
@@ -10,16 +10,42 @@ const QueryInput = ({setChatBubbles}: {setChatBubbles: Function}) => {
     }
 
     const [query, setQuery] = useState('');
+    const [isFetchingBotResponse, setIsFetchingBotResponse] = useState(false);
+
+    useEffect(() => {
+        if (!isFetchingBotResponse) return;
+
+        fetch("http://localhost:8000/query", {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify({query: query})
+        })
+        .then(res => res.json())
+        .then(json => setChatBubbles((prev: ChatBubbleProps[]) => {
+            console.log(json.response)
+            prev[prev.length - 1] = json.response;
+            setChatBubbles(prev);
+            setQuery('');
+            setIsFetchingBotResponse(false);
+        }));
+        
+    }, [isFetchingBotResponse]);
 
     function handleOnClick() {
         if (query) {
             // send query to api
 
+
             // show user chat bubble with user query
-            setChatBubbles((prev: ChatBubbleProps[]) => [...prev, {start: false, text: query}]);
+            setChatBubbles((prev: ChatBubbleProps[]) => [...prev, {start: false, text: query}, {start: true, text: ""}]);
 
             // reset query
-            setQuery('');
+            // setQuery('');
+
+            // set fetiching response
+            setIsFetchingBotResponse(true);
         }
     }
 
