502bf3b302efefc6fa146fdcd51a1dcea0266eb2
Raul Andrial <rauljandrial@gmail.com>
2024-08-25 19:49:40 -0400

    scrolls to the bottom automatically when new message is added to chat window or bot response is updated

--- app/server_components/ChatbotHistory.tsx
+++ app/server_components/ChatbotHistory.tsx
@@ -1,14 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import ChatBubble from './ChatBubble';
 import { ChatBubbleProps } from '../types/types';
 import ChatbotContext from '../context/ChatbotContext';
 
 const ChatbotHistory = () => {
 
+  const chatHistoryRef = useRef<HTMLInputElement>(null);
   const {chatbotState} = useContext(ChatbotContext);
   const {chatHistory} = chatbotState;
+  const lastMsg = chatHistory.length ? chatHistory[chatHistory.length - 1].text : '';
+
+  function scrollToBottom() {
+    if (chatHistoryRef.current) {
+      const ele = chatHistoryRef.current;
+      ele.scrollTop = ele.scrollHeight;
+    }
+  }
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatHistory, lastMsg]);
+
   return (
-    <div className="max-md:flex max-md:flex-col lg:card lg:card-compact bg-slate-100 w-full md:shadow-xl lg:mt-7 lg:mb-3 grow overflow-y-scroll max-md:mb-[17vh] p-1">
+    <div 
+      className="max-md:flex max-md:flex-col lg:card lg:card-compact bg-slate-100 w-full md:shadow-xl lg:mt-7 lg:mb-3 grow overflow-y-scroll max-md:mb-[17vh] p-1"
+      ref={chatHistoryRef}
+    >
       <ChatBubble start text="It was said that you would, destroy the Sith, not join them."/>
       <ChatBubble start={false} text="It was you who would bring balance to the Force"/>
       <ChatBubble start text=""/>
